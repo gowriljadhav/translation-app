df17afabc9f7ec52acd6f54d9ca154390279d00b
gowriljadhav <75152052+gowriljadhav@users.noreply.github.com>
2020-12-07 16:30:06 +0530

    minor fixes

--- app.js
+++
@@ -1,7 +0,0 @@
-console.log("script is working from a different location");
-
-var username = prompt("Give your username");
-
-var welcomemessage = "This script works "+ username;
-
-alert(welcomemessage); 
