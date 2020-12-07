fff014dd4eea2df8dc42890223dd5b20a25da114
gowriljadhav <75152052+gowriljadhav@users.noreply.github.com>
2020-12-07 15:06:11 +0530

    imported javascript

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("script is working from a different location");
+
+var username = prompt("Give your username");
+
+var welcomemessage = "This script works "+ username;
+
+alert(welcomemessage); 
